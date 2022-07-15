f637b26a508514f1c9b979dbc5d78e0e4a7e72b2
fanmiao11 <miao623@outlook.com>
2022-07-15 17:52:57 +0800

    完成布局页面路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,29 @@ const routes = [
     path: '/login',
     // component:Login,
     component: () => import('@/views/Login')
+  },
+  {
+    path: '/',
+    component: () => import('@/views/Layout'),
+    redirect: '/ ',
+    children: [
+      {
+        path: '/ ',
+        component: () => import('@/views/Home')
+      },
+      {
+        path: '/video',
+        component: () => import('@/views/Video')
+      },
+      {
+        path: '/qa',
+        component: () => import('@/views/QA')
+      },
+      {
+        path: '/profile',
+        component: () => import('@/views/My')
+      }
+    ]
   }
 ]
 
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <router-view></router-view>
+    <van-tabbar route>
+      <van-tabbar-item to="/ ">
+        <template #icon>
+          <span class="toutiao toutiao-shouye"></span>
+        </template>
+          <span>首页</span>
+      </van-tabbar-item>
+      <van-tabbar-item to="/video">
+        <template #icon>
+          <span class="toutiao toutiao-shipin"></span>
+        </template>
+        <span>视频</span>
+      </van-tabbar-item>
+      <van-tabbar-item to="/qa">
+        <template #icon>
+          <span class="toutiao toutiao-wenda"></span>
+        </template>
+      <span>问答</span>
+      </van-tabbar-item>
+      <van-tabbar-item to="/profile">
+        <template #icon>
+          <span class="toutiao toutiao-wode"></span>
+        </template>
+        <span>我的</span>
+      </van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    //   active: 0
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+:deep(.van-tabbar-item__icon) {
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+.toutiao{
+    font-size: 0.53333rem;
+}
+}
+</style>
